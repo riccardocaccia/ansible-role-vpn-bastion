715bc8e3bcc529581cae2a9ca173b89f043f7746
Ubuntu <ubuntu@ansible-role-vpn-bastion.recasbari>
2025-12-02 11:50:47 +0000

    last pprint and mod. to variables file

--- terraform_bastion/terraform.tfvars
+++ terraform_bastion/terraform.tfvars
@@ -1,9 +1,10 @@
-auth_url      = ""
-user_name     = ""
-password      = ""
-tenant_name   = ""
-region        = "RegionOne"
+# DO NOT COMMIT THIS FILE
+auth_url      = "authentication_url"
+user_name     = "my_user"
+password      = "my_super_secret_password"
+tenant_name   = "name"
+region        = "region_name"
 
 public_network = "public"
-flavor         = "small"
+flavor         = "desired_flavour"
 image          = "Ubuntu 22.04"
--- terraform_bastion/variables.tf
+++ terraform_bastion/variables.tf
@@ -1,8 +1,40 @@
-variable "auth_url" {}
-variable "user_name" {}
-variable "password" {}
-variable "tenant_name" {}
-variable "region" {}
-variable "public_network" {}
-variable "flavor" {}
-variable "image" {}
+variable "auth_url" {
+  type = string
+  description = "OpenStack Keystone v3 auth URL"
+}
+
+variable "user_name" {
+  type      = string
+  description = "OpenStack username"
+}
+
+variable "password" {
+  type      = string
+  sensitive = true
+  description = "OpenStack password"
+}
+
+variable "tenant_name" {
+  type = string
+  description = "OpenStack tenant name"
+}
+
+variable "region" {
+  type = string
+  description = "OpenStack region"
+}
+
+variable "public_network" {
+  type        = string
+  description = "openStack public network"
+}
+
+variable "flavor" {
+  type        = string
+  description = "OpenStack flavor for Galaxy VM"
+}
+
+variable "image" {
+  type        = string
+  description = "OpenStack image name for Galaxy VM"
+}
